ccd2296ffc7fcc6c0df9ca22d9807d74d43eeccc
ihor <ihor04@gmail.com>
2020-04-03 23:46:23 +0300

    create Task4.js .***

--- Task4.js
+++ Task4.js
@@ -171,3 +171,127 @@ function cars (car, id) {
     }
 }
 cars(vehicles, 'Car');
+//
+// - *** приймає масив та число "i", та міняє місцями об`єкт який знаходиться в індексі "i" на "i+1"
+let vehicles = [
+    {id: 1, model: 'Ford', year: 2015, power: 2.5, color: 'red'},
+    {id: 2, model: 'Toyota', year: 2018, power: 2.0, color: 'white'},
+    {id: 3, model: 'Mercedes', year: 2005, power: 2.8, color: 'black'},
+    {id: 4, model: 'BMW', year: 2010, power: 2.2, color: 'darkgrey'},
+    {id: 5, model: 'Lada', year: 1966, power: 1.3, color: 'rusty'},
+    {id: 6, model: 'WolksVagen', year: 2014, power: 1.5, color: 'brown'},
+    {id: 7, model: 'Smart', year: 2019, power: 1.2, color: 'pink'},
+    {id: 8, model: 'Electron', year: 2020, power: 12.5, color: 'yellow'},
+    {id: 9, model: 'Tesla', year: 2013, power: 1.7, color: 'silver'},
+    {id: 10, model: 'Hummer', year: 2012, power: 5.5, color: 'purple'},
+];
+function changePosition(anyArray, index){
+    // console.log(anyArray[index])
+    anyArray.splice(index, 0, anyArray[index+1])
+    anyArray.splice(index+2, 1)
+    return console.log(anyArray)
+}
+changePosition(vehicles, 1)
+changePosition(vehicles, 3)
+// // - *** створити функцію яка буде переносити елементи з значенням 0 у кінець маисву. Зберігаючи при цьому порядок не нульових значень.
+// Двожина масиву від 2 до 100
+let array = [];
+let zerosArray = [];
+let countZero = 0;
+let arrayLength = Math.round(Math.random() * 98) + 2;
+console.log(arrayLength)
+function awayZeros(){
+    for (let i = 0; i < arrayLength ; i++) {
+        let number = Math.floor(Math.random() * 3);
+        if (number === 0){
+            zerosArray.push(number);
+            countZero += 1;
+        }else {
+            array[i - countZero] = number;
+        }
+    }
+    for (each of zerosArray){
+        array[array.length]  = each;
+    }
+    console.log(array);
+    console.log(zerosArray)
+}
+awayZeros();
+// Приклад
+// [1,0,6,0,3] => [1,6,3,0,0]
+// [0,1,2,3,4] => [1,2,3,4,0]
+// [0,0,1,0]   => [1,0,0,0]
+//(на основі минулого ДЗ)
+// **- функція приймає 2 масиви з рівною кількістю об'єктів та з'єднює в один об'єкт користувача та місто з відповідними "id" та "user_id",
+// та повертає масив цих з'єднаних об'єктів.
+usersWithCities = [];
+let usersWithId = [
+    {id: 1, name: 'vasya', age: 31, status: false},
+    {id: 2, name: 'petya', age: 30, status: true},
+    {id: 3, name: 'kolya', age: 29, status: true},
+    {id: 4, name: 'olya', age: 28, status: false},];
+let citiesWithId = [
+    {user_id: 3, country: 'USA', city: 'Portland'},
+    {user_id: 1, country: 'Ukraine', city: 'Ternopil'},
+    {user_id: 2, country: 'Poland', city: 'Krakow'},
+    {user_id: 4, country: 'USA', city: 'Miami'},];
+let usersSities = [];
+function combineArrays() {
+    for (let i = 0; i < usersWithId.length; i++){
+        for (let j = 0; j < citiesWithId.length; j++){
+            if (usersWithId[i].id === citiesWithId[j].user_id){
+                usersWithId[i].address = citiesWithId[j];
+            }
+        }
+        usersSities.push(usersWithId[i])
+    }
+    console.log(usersSities);
+}
+combineArrays();
+
+//
+//
+//
+// ***- беремо завдання з правилами з укроку №3 :
+// Та робимо це функцією.При цьому правила отримувати через аргумент.
+// "Є масив котрий характеризує правила. Створити скрипт який ітерує цей масив, та робить з кожне правило в окремому блоці.
+// При цому в блоці, номер правила записати в свій блок, текст правила записати в свій окремий блок.
+// Результатом відпрацювання скріпта повинна бути структура яка міститься в блоці wrap файла rule.html
+// //todo add rules
+//
+// 			let rules = [
+// 				{
+// 					title: 'Первое правило Бойцовского клуба.',
+// 					body: 'Никому не рассказывать о Бойцовском клубе.'
+// 				},
+// 				{
+// 					title: 'Второе правило Бойцовского клуба.',
+// 					body: 'Никогда никому не рассказывать о Бойцовском клубе.'
+// 				},
+// 				{
+// 					title: 'Третье правило Бойцовского клуба.',
+// 					body: 'В схватке участвуют только двое.'
+// 				},
+// 				{
+// 					title: 'Четвертое правило Бойцовского клуба.',
+// 					body: 'Не более одного поединка за один раз.'
+// 				},
+// 				{
+// 					title: 'Пятое правило Бойцовского клуба.',
+// 					body: 'Бойцы сражаются без обуви и голые по пояс.'
+// 				},
+// 				{
+// 					title: 'Шестое правило Бойцовского клуба.',
+// 					body: 'Поединок продолжается столько, сколько потребуется.'
+// 				},
+// 				{
+// 					title: 'Седьмое правило Бойцовского клуба.',
+// 					body: 'Если противник потерял сознание или делает вид, что потерял, или говорит «Хватит» — поединок окончен.'
+// 				},
+// 				{
+// 					title: 'Восьмое и последнее правило Бойцовского клуба.',
+// 					body: 'Новичок обязан принять бой.'
+// 				},
+//
+// 			];
+// "
